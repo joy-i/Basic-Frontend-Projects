1a63207fa6c983f728b4622ffbd554977e10e434
joy-i <77374152+joy-i@users.noreply.github.com>
2021-06-12 23:58:37 +0530

    Add files via upload

---
+++ Gallery/Gallery2/my-gallary.js
@@ -0,0 +1,41 @@
+//swiper.js
+
+new Swiper(".swiper-container", {
+    speed: 1000,
+    spaceBetween: 100,
+    effect:"swipe",
+    slidesPerView:2,
+    loop:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+ }
+});
+
+//popmotion.js (navigation physics)
+
+const { styler, spring, listen, pointer, value } = window.popmotion;
+
+
+
+const ball = document.querySelector('.brand');
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, 'mousedown touchstart')
+  .start((e) => {
+    e.preventDefault();
+    pointer(ballXY.get()).start(ballXY); 
+  });
+
+listen(document, 'mouseup touchend')
+  .start(() => {
+    spring({
+      from: ballXY.get(),
+      velocity: ballXY.getVelocity(),
+      to: { x: 0, y: 0 },
+      stiffness: 200,
+      // mass: 1,
+      // damping: 10
+    }).start(ballXY);
+  });
